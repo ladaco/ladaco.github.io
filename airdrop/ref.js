67912f7fcbc6d7d4c6c1685cc3538db392dc738e
ladaco <euge@email.ua>
2021-07-05 03:18:40 +0300

    Add files via upload

--- airdrop/ref.js
+++ airdrop/ref.js
@@ -1,8 +1,8 @@
     function getreflink(){
       var refaddr = document.getElementById("refaddress").value;
       var reflink = document.getElementById("refht");
-      reflink.href = '/airdrop/?ref=' + refaddr;
-      reflink.innerHTML = '/airdrop/?ref=' + refaddr;
+      reflink.href = 'https://ladaco.github.io/airdrop/?ref=' + refaddr;
+      reflink.innerHTML = 'https://ladaco.github.io/airdrop/?ref=' + refaddr;
       document.getElementById('refs').style.display = 'block';
     }
     function copyToClipboard(id) {
